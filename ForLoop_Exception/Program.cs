ce4b9aeaf3caccdea8ca6f123cca33d71663c1af
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-25 17:37:08 -0400

    Add project files.

---
+++ ForLoop_Exception/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForLoop_Exception
+{
+    class Program
+    {
+        //for loop 
+        static void Main(string[] args)
+        {
+            for(int i = 6; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] luckyNumbers = { 4, 8, 10, 13, 24 };
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+        }
+    }
+}
