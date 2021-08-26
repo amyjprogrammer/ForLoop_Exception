09a16a70184b730dc2f257a472202a3ca46e1e3f
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-26 19:12:00 -0400

    Exponent Method and 2d Arrays

--- ForLoop_Exception/Program.cs
+++ ForLoop_Exception/Program.cs
@@ -17,6 +17,32 @@ namespace ForLoop_Exception
             {
                 Console.WriteLine(luckyNumbers[i]);
             }
+
+            Console.WriteLine(GetPow(3, 3));
+            Console.WriteLine(GetPow(4, 3));
+
+            //2d Array
+            int[,] numberGrid =
+            {
+                {1, 2 },
+                {3, 4 },
+                {5, 6 }
+            };
+
+            Console.WriteLine(numberGrid[2, 1]); //the third one down and the second one (6)
+            Console.WriteLine(numberGrid[0,0]);//1, because it is the first one down and first number
+        }
+        //Exponent Method
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for(int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+            return result;
         }
     }
 }
