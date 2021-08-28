725f40391657a39451a1d6e0204843d747c120a3
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-27 21:52:50 -0400

    Book Class and Objects

--- ForLoop_Exception/Program.cs
+++ ForLoop_Exception/Program.cs
@@ -31,7 +31,24 @@ namespace ForLoop_Exception
 
             Console.WriteLine(numberGrid[2, 1]); //the third one down and the second one (6)
             Console.WriteLine(numberGrid[0,0]);//1, because it is the first one down and first number
+
+            ExcHandling();
+
+            //creating an object for Class Book
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book1.title);
         }
+
         //Exponent Method
         static int GetPow(int baseNum, int powNum)
         {
@@ -44,5 +61,46 @@ namespace ForLoop_Exception
 
             return result;
         }
+
+        //Exception Handling
+        static void ExcHandling()
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+
+            //this will show the error message and why it broke
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            try
+            {
+                Console.Write("Enter a number: ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(number1);
+            }
+            //handling the different exceptions
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
     }
 }
