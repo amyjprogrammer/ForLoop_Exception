223d71f67db3fa7dfb6569415e9539bfd1e5a020
amyjprogrammer <amyjprogrammer@gmail.com>
2021-09-02 17:31:22 -0400

    Getters and Setters

---
+++ getters_setters/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getters_setters
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating; //makes it so this can only be accessed in the Movie class and can't be changed
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;  //changed to the method below so person can't put anything they want for rating
+        }
+
+        public string Rating 
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value =="NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            } 
+        }
+
+    }
+}
---
+++ getters_setters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getters_setters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "dog");//wrong value for rating
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            shrek.Rating = "PG-13";
+
+            Console.WriteLine(shrek.Rating);//updated to PG-13, because that is a valid input
+
+            Console.WriteLine(avengers.Rating); //sets it to NR instead
+        }
+    }
+}
