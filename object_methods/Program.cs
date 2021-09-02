a02f7be67bbb410faf2be77caca927b5829055db
amyjprogrammer <amyjprogrammer@gmail.com>
2021-09-02 17:06:27 -0400

    Object methods

---
+++ object_methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students student1 = new Students("Jim", "Business", 2.8);
+            Students student2 = new Students("Pam", "Art", 3.8);
+
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+        }
+    }
+}
---
+++ object_methods/Students.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_methods
+{
+    class Students
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Students(string aName, string aMajor, double aGPA)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGPA;
+        }
+
+        public bool HasHonors()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
